cb874f4fae0c2e55c0d61c599857dc2d889064fe
kasunmadu12345 <kasunmadu12@gmail.com>
2022-12-16 13:32:10 +0530

    Test part 01-UI PART

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,55 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ChakraProvider, Grid, GridItem } from '@chakra-ui/react';
+import { Heading } from '@chakra-ui/react';
+import { Textarea } from '@chakra-ui/react';
+import { Flex, Spacer } from '@chakra-ui/react';
+import { Button, ButtonGroup } from '@chakra-ui/react';
+
+import cbor from 'cbor' 
+import fs from 'fs'
 
 function App() {
+const Buttonclick=()=>{
+console.log('Kasun M'); 
+  const d = new cbor.Decoder()
+d.on('data', (obj:any) => {
+  console.log(obj)
+})
+
+const s = fs.createReadStream('foo')
+s.pipe(d)
+
+//const d2 = new cbor.Decoder({input: '00', encoding: 'hex'})
+d.on('data', (obj:any) => {
+  console.log(obj)
+})
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+  <Heading color={"Blue"} >
+CBOR ENcoding System 
+
+  </Heading>
+
+<h1>Enter Text Below </h1>
+  <h1> Diagnostic</h1>
+  <Grid templateColumns='repeat(5, 1fr)' gap={4}>
+  <GridItem colSpan={2} h='10' >
+  <Button colorScheme='blue'>Open Text</Button>
+
+<Textarea height={500}  placeholder='Test'   />
+  </GridItem>
+  <GridItem   colSpan={2}  h='10' >
+
+  <Button colorScheme='blue'>Open Text</Button>
+  <Textarea height={500}    placeholder='Test2'   />
+
+  </GridItem>
+</Grid>
+
+    </ChakraProvider>
   );
 }
-
 export default App;
