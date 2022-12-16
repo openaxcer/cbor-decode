6f38aa71f37555569a5fce6dcb46ecf47ec887c7
kasunmadu12345 <kasunmadu12@gmail.com>
2022-12-17 00:52:40 +0530

    Stable version 2

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
-//import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 import { ChakraProvider, Grid, GridItem } from '@chakra-ui/react';
 import { Heading } from '@chakra-ui/react';
 import { Textarea } from '@chakra-ui/react';
 import { Flex, Spacer } from '@chakra-ui/react';
 import { Button, ButtonGroup } from '@chakra-ui/react';
 
-function App () {
+function App() {
+  const [encodedData, setEncodedData] = useState("");
+  const [decodedData, setDecodedData] = useState("");
+
+  const onEncode = () => {
+
+    fetch("/getcode", {
+      method: 'POST', headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        data: encodedData,
+        isencoded: true
+      })
+    }).then((res) => {
+      return res.json();
+    }).then((resp) => {
+      setDecodedData(resp.data.data.toString());
+
+    }).catch((err) => {
+      console.log(err.message);
+    })
+
+  }
   return (
     <ChakraProvider>
       <Heading color={"Blue"} >
@@ -19,14 +38,14 @@ function App () {
       <h1> Diagnostic</h1>
       <Grid templateColumns='repeat(5, 1fr)' gap={4}>
         <GridItem colSpan={2} h='10' >
-          <Button colorScheme='blue'>Open Text</Button>
+          <Button colorScheme='blue' onClick={onEncode}>Encode</Button>
 
-          <Textarea height={500} placeholder='Test' />
+          <Textarea value={encodedData} onChange={(e) => setEncodedData(e.target.value)} height={500} placeholder='Test' />
         </GridItem>
         <GridItem colSpan={2} h='10' >
 
-          <Button colorScheme='blue'>Open Text</Button>
-          <Textarea height={500} placeholder='Test2' />
+          <Button colorScheme='blue'>Decode</Button>
+          <Textarea value={decodedData} onChange={(e) => setDecodedData(e.target.value)} height={500} placeholder='Test2' />
 
         </GridItem>
       </Grid>
